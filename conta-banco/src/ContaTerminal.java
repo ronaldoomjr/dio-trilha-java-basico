ac76f67eb39dbb75a5c0f76fa736ed906a75c285
ronaldoomjr <ronaldoomjunior@gmail.com>
2024-06-04 23:50:27 -0300

    initial commit

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        // TODO: Conhecer e importar a classe Scanner
+
+        // TODO: Exibir as mensagens para o nosso usuário
+
+        // TODO: Obter pela classe Scanner os valores digitados no terminal
+
+        // TODO: Exibir a mensagem conta criada
+    }
+}
